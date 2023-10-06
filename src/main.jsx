856bb0e6adc5fbf54c9cb70d40a4142ab9da6598
Kevin Carrillo <kcarrillo@joonik.com>
2023-10-05 19:45:14 -0500

    init project

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { CounterApp } from './01-useState/CounterApp'
+// import { HooksApp } from './HooksApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CounterApp />
+    {/* <HooksApp /> */}
+  </React.StrictMode>,
+)
