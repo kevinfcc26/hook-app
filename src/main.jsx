15c0b016b89fa9be47b0be607ee460fa23f32d83
Kevin Carrillo <kcarrillo@joonik.com>
2023-10-06 16:57:02 -0500

    useLayoutEffect

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { FocusScreen } from './04-useRef/FocusScreen'
+import { Layout } from './05-useLayoutEffect/Layout'
+// import { FocusScreen } from './04-useRef/FocusScreen'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { SimpleFormWithCustomHook } from './02-useEffect/SimpleFormWithCustomHook'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
@@ -11,7 +12,8 @@ import { FocusScreen } from './04-useRef/FocusScreen'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FocusScreen />
+    <Layout />
+    {/* <FocusScreen /> */}
     {/* <MultipleCustomHooks /> */}
     {/* <SimpleFormWithCustomHook /> */}
     {/* <SimpleForm /> */}
