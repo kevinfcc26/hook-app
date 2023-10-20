97f17cba6b8102e404344c4d22b8f2702a6b9bc9
kevinfcc26 <kevinfcc26@gmail.com>
2023-10-20 15:10:51 -0500

    create navbar

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,11 +1,14 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AboutPage, HomePage, LoginPage } from "./";
+import { Navbar } from "./Navbar";
 
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
+      <Navbar />
       <hr />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="about" element={<AboutPage />} />
