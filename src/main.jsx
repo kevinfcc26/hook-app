48e8ab3058c4807b101f0762f29cb928de742392
kevinfcc26 <kevinfcc26@gmail.com>
2023-10-20 10:51:05 -0500

    add react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 // import { CallbackHook } from "./06-memos/CallbackHook";
 // import { MemoHook } from './06-memos/MemoHook'
 // import { Memorize } from './06-memos/Memorize'
@@ -14,22 +15,26 @@ import "./index.css";
 // import { HooksApp } from './HooksApp'
 // import { Padre } from "./07-tarea/Padre";
 // import "./08-useReducer/intro-reducer";
-import { TodoApp } from "./08-useReducer/TodoApp";
+// import { TodoApp } from "./08-useReducer/TodoApp";
+import { MainApp } from "./09-useContext/MainApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <TodoApp />
-    {/* <Padre /> */}
-    {/* <CallbackHook /> */}
-    {/* <MemoHook /> */}
-    {/* <Memorize /> */}
-    {/* <Layout /> */}
-    {/* <FocusScreen /> */}
-    {/* <MultipleCustomHooks /> */}
-    {/* <SimpleFormWithCustomHook /> */}
-    {/* <SimpleForm /> */}
-    {/* <CounterWithCustomHook /> */}
-    {/* <CounterApp /> */}
-    {/* <HooksApp /> */}
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <MainApp />
+      {/* <TodoApp /> */}
+      {/* <Padre /> */}
+      {/* <CallbackHook /> */}
+      {/* <MemoHook /> */}
+      {/* <Memorize /> */}
+      {/* <Layout /> */}
+      {/* <FocusScreen /> */}
+      {/* <MultipleCustomHooks /> */}
+      {/* <SimpleFormWithCustomHook /> */}
+      {/* <SimpleForm /> */}
+      {/* <CounterWithCustomHook /> */}
+      {/* <CounterApp /> */}
+      {/* <HooksApp /> */}
+    </React.StrictMode>
+  </BrowserRouter>
 );
